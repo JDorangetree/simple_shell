389b39a9e9244030586c05a9db25f8002e9d132d
JDorangetree <julian.naranjo2014@gmail.com>
2019-11-18 22:22:07 +0000

    upload 2

--- orangesh.c
+++ orangesh.c
@@ -1,10 +1,12 @@
 #include "orange.h"
 
-int main (void)
+int main (char *argv)
 {
     char *buffer, **input_array;
     size_t size_bufer = 1;
     ssize_t read;
+    pid_t pid_C;
+    int number, exc;
 
     printf("$ ");
     buffer = malloc(sizeof(char)* size_bufer);
@@ -12,10 +14,24 @@ int main (void)
         return(0);
     while ((read = getline(&buffer, &size_bufer, stdin)) != -1)
     {
+        pid_C = fork();
+        if (pid_C == -1)
+        {
+            perror("Error:");
+            return(1);
+        }
         input_array = strtow(buffer);
-        execve(input_array[0], input_array, NULL);
+        if (pid_C == 0)
+            exc = execve(input_array[0], input_array, NULL);
+                if (exc == -1)
+                    printf("%s: No such file or directory\n$", argv);
+
+        else
+        {
+            wait(&number);
+            printf("$ ");
+        }
     }
-    printf("$ ");
     free(buffer);
     return(read);
 }
